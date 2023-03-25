8072136191aa5c369ea982017c0d2423e0c0daca
sukrutha3112 <sukrutha3112@gmail.com>
2023-03-25 20:05:30 +0530

    Components added

---
+++ src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/system'
+
+const Feed = () => {
+  return (
+    <Stack>
+
+        
+    </Stack>
+  )
+}
+
+export default Feed
